9aaedd8aa1421827aa1d6dcdbf4cf5f49d4a4fd3
Xin Zhang <xinzhang@192-168-1-125.tpgi.com.au>
2020-03-05 21:49:06 +1100

    fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,35 +1,34 @@
-<template type="text/x-template" id="card-template">
-  <div>
-    <h3>{{title}}</h3>
-    <ul>
-      <li v-for="(link,idx) in links" :key="idx">
-        <a :href="link.url">{{link.text}}</a>
-      </li>
-    </ul>
-  </div>
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <ul>
+            <li v-for="(link, idx) in links" :key="idx">
+                <a :href="link.url">{{ link.text }}</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "CardItem",
-  template: "#card-template",
-  props: ["title", "links"]
+    name: 'CardItem',
+    props: ['title', 'links'],
 };
 </script>
 
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
